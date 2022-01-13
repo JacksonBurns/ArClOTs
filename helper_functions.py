2552b3d51e954affd90041700f32565b89d48a9c
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-01-13 14:35:25 -0500

    dummy models done

---
+++ helper_functions.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def prediction_error_plot(actual, predicted, title=None):
+    fig, ax = plt.subplots(figsize=(6,6))
+    ax.scatter(actual, predicted, edgecolors=(0, 0, 0))
+    ax.plot([actual.min(), actual.max()], [actual.min(), actual.max()], "k--", lw=4)
+    ax.set_xlabel("Actual")
+    ax.set_ylabel("Predicted")
+    if title:
+        plt.title(title)
+    plt.show()
