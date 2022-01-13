3831a94a09d045b1887e2b2e09e117e4e956b226
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-01-13 16:30:00 -0500

    part 1 done

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
 def prediction_error_plot(actual, predicted, title=None):
+#     actual = 2.718**actual
+#     predicted = 2.718**predicted
     fig, ax = plt.subplots(figsize=(6,6))
     ax.scatter(actual, predicted, edgecolors=(0, 0, 0))
     ax.plot([actual.min(), actual.max()], [actual.min(), actual.max()], "k--", lw=4)
